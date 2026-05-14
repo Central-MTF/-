4987ea7aaa3fb58815b711aa4d089e738f6d4136
T-rex-pro <saidmascorro11@gmail.com>
2026-05-13 18:14:30 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("MTF-HQ SYSTEM ONLINE");
+
+setInterval(() => {
+
+document.title =
+document.title === "Cuartel General MTF"
+?
+"[ ALERTA ] Cuartel General MTF"
+:
+"Cuartel General MTF";
+
+}, 3000);
